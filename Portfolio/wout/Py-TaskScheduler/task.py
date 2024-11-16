1da7b983de04151ad09d129d110aba4c806b5f47
euphratesk <manaiharfi.kf@hotmail.com>
2024-11-16 23:12:33 +0300

    'as11-16'

---
+++ Portfolio/wout/Py-TaskScheduler/task.py
@@ -0,0 +1,5 @@
+import datetime 
+
+file = open(r'C:\Users\Admin\OneDrive\Desktop\TECHPRO\GitPortf\Portfolio\wout\Py-TaskScheduler\tasx.txt','a')
+
+file.write(f'{datetime.datetime.now()} - The script ran \n')
