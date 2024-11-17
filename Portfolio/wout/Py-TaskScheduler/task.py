ad2a86c83b40a6b1a3af2145e90d993aee0f6e5e
euphratesk <manaiharfi.kf@hotmail.com>
2024-11-17 23:29:34 +0300

    'as11-17'

--- Portfolio/wout/Py-TaskScheduler/task.py
+++ Portfolio/wout/Py-TaskScheduler/task.py
@@ -2,4 +2,4 @@ import datetime
 
 file = open(r'C:\Users\Admin\OneDrive\Desktop\TECHPRO\GitPortf\Portfolio\wout\Py-TaskScheduler\tasx.txt','a')
 
-file.write(f'{datetime.datetime.now()} - The script ran \n')
+file.write(f"{datetime.datetime.now()} - The script ran \n")
